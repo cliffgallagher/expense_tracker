80439554a4d57b82f6b0a936234037664e27e8bc
cliffgallagher <scliffordgallagher@gmail.com>
2021-07-07 18:19:39 -0500

    app adds expenses to list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,11 +7,10 @@ const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
   return (
-    console.log(props),
     <Card className="expense-item">
       <ExpenseDate date={ props.date }/>
       <div className="expense-item__description">
-        <h2>{ title }</h2>
+        <h2>{ props.title }</h2>
         <div className="expense-item__price">{ props.amount }</div>
       </div>
     </Card>
