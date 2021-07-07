0a47e6539e5872960f3f768bd9a9ad7d128d1af4
cliffgallagher <scliffordgallagher@gmail.com>
2021-07-07 06:17:40 -0500

    addExpense handler adds expense to expenses array

--- src/App.js
+++ src/App.js
@@ -27,13 +27,14 @@ const App = () => {
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
-    },
+    }
   ]
 
   const addExpenseHandler = expense => {
     console.log('In App.js');
-    console.log(expense
-      );
+    //console.log(expense);
+    expenses.unshift(expense);
+    console.log(expenses);
   }
   
   return (
